dfecddc697840ecc64b511c2145e99f9abb19802
TommoParra <tomas.moralparra@gmail.com>
2025-01-17 12:02:45 +0100

    Some progress

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,59 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import EventCard from '@/components/EventCard.vue'
+import { ref } from 'vue'
+
+const events = ref([
+  {
+    id: 5928101,
+    category: 'animal welfare',
+    title: 'Cat Adoption Day',
+    description: 'Find your new feline friend at this event.',
+    location: 'Meow Town',
+    date: 'January 28, 2022',
+    time: '12:00',
+    petsAllowed: true,
+    organizer: 'Kat Laydee',
+  },
+  {
+    id: 4582797,
+    category: 'food',
+    title: 'Community Gardening',
+    description: 'Join us as we tend to the community edible plants.',
+    location: 'Flora City',
+    date: 'March 14, 2022',
+    time: '10:00',
+    petsAllowed: true,
+    organizer: 'Fern Pollin',
+  },
+  {
+    id: 6734928,
+    category: 'technology',
+    title: 'Echo Tech Meetup',
+    description:
+      'Explore the latest innovations in sustainability and network with tech enthusiasts.',
+    location: 'Silicon Valley',
+    date: 'February 20, 2025',
+    time: '15:00',
+    petsAllowed: false,
+    organizer: 'Tech Innovators Group',
+  },
+])
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>Events For Good</h1>
+  <div class="home">
+    <div class="events">
+      <EventCard v-for="event in events" :key="event.id" :event="event" />
+    </div>
+  </div>
+  <EventCard />
 </template>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
